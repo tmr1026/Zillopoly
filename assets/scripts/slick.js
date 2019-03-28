17c64db2c58fef7a5b79e29178f3ea1395b6d21c
Will Woods <wwoods1016@gmail.com>
2019-03-27 21:15:08 -0400

    app.js fixes

--- assets/scripts/slick.js
+++ assets/scripts/slick.js
@@ -1,16 +1,16 @@
-// $(document).ready(function () {
+$(document).ready(function () {
     
-//     let APIimages = "<img src='" + homeImage + "'alt='Image of home from Zillow API'/>"
-//     $(".slider-for").slick("slickAdd", APIimages);
-//     $('#images').slick({
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         autoplay: true,
-//         autoplaySpeed: 2000,
-//         dots: true,
-//         infinite: true,
-//         speed: 500,
-//         fade: true,
-//         cssEase: 'linear'
-//     });
-// });
+    let APIimages = "<img src='" + imgArr + "'alt='Image of home from Zillow API'/>"
+    $(".slider-for").slick("slickAdd", APIimages);
+    $('#images').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
